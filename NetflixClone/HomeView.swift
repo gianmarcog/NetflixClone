948bb6390c1ec4b824d8c53cdf9c9f766e7763fe
gianmarcog <marco.giretti@web.de>
2020-09-10 02:12:16 +0200

    Create Project

---
+++ NetflixClone/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NetflixClone
+//
+//  Created by Gianmarco Giretti on 10.09.20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
