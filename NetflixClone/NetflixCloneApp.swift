a4a7c0466c6b97a68fc2fccf5ae8900cf3039837
gianmarcog <marco.giretti@web.de>
2020-09-10 02:10:55 +0200

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Gianmarco Giretti on 10.09.20.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
