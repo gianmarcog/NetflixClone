b55fbb3effb4d487a89c4e94479a8b3c00ec74ca
gianmarcog <marco.giretti@web.de>
2020-09-16 03:20:08 +0200

    Added preview for the HomeView

--- NetflixClone/HomeView.swift
+++ NetflixClone/HomeView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct HomeView: View {
+    var vm = HomeVM()
+    
+    // Gibt die Grenzen des iOS-Geräts zurück
+    let screen = UIScreen.main.bounds
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            // mai VStack
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    
+                    // Anzeige von TopMoviePreview für die View
+                    TopMoviePreview(movie: exampleMovie3)
+                        .frame(width: screen.width)
+                        .padding(.top, -110)
+                    
+                    // Alle Kategorien werden gelistet
+                    ForEach(vm.allCategories, id: \.self) { category in
+                        //Alle Kategorien werden nochmals in einem VStach sortiert
+                        VStack {
+                            HStack {
+                                Text(category)
+                                    .font(.title3)
+                                    .bold()
+                                Spacer()
+                            }
+                            
+                            // Damit Filme einer Kategorie horizontal gewischt werden können
+                            ScrollView(.horizontal, showsIndicators: false){
+                                LazyHStack {
+                                    ForEach(vm.getMovie(forCat: category)) {
+                                        movie in
+                                        StandardHomeMovie(movie: movie)
+                                            .frame(width: 100, height: 200)
+                                            .padding(.horizontal, 30)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
