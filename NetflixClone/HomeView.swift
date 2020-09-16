7e2e29753c5318bf804c94246efa8561daa8a5ac
gianmarcog <marco.giretti@web.de>
2020-09-16 19:03:26 +0200

    create and move TopRowButtons

--- NetflixClone/HomeView.swift
+++ NetflixClone/HomeView.swift
@@ -23,10 +23,13 @@ struct HomeView: View {
             ScrollView(showsIndicators: false) {
                 LazyVStack {
                     
+                    TopRowButtons()
+                    
                     // Anzeige von TopMoviePreview für die View
                     TopMoviePreview(movie: exampleMovie3)
                         .frame(width: screen.width)
                         .padding(.top, -110)
+                        .zIndex(-1)
                     
                     // Alle Kategorien werden gelistet
                     ForEach(vm.allCategories, id: \.self) { category in
@@ -62,5 +65,50 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+struct TopRowButtons: View {
+    var body: some View {
+        HStack{
+            Button(action: {
+                //
+            }, label: {
+                Image("netflix_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50)
+            })
+            .buttonStyle(PlainButtonStyle())
+            
+            Spacer()
+            
+            Button(action: {
+                //
+            }, label: {
+                Text("TV Show")
+            })
+            .buttonStyle(PlainButtonStyle())
+            
+            Spacer()
+            
+            Button(action: {
+                //
+            }, label: {
+                Text("Movies")
+            })
+            .buttonStyle(PlainButtonStyle())
+            
+            Spacer()
+            
+            Button(action: {
+                //
+            }, label: {
+                Text("My List")
+            })
+            .buttonStyle(PlainButtonStyle())
+        }
+        .padding(.leading, 10)
+        .padding(.trailing, 20)
     }
 }
