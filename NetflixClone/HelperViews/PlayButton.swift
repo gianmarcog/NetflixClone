65e0cfd6a0bd06db81ed588a72129c25b6e07e93
gianmarcog <marco.giretti@web.de>
2020-09-18 13:55:50 +0200

    Change the name of the struct

--- NetflixClone/HelperViews/PlayButton.swift
+++ NetflixClone/HelperViews/PlayButton.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-struct WhiteButton: View {
+struct PlayButton: View {
     
     var text: String
     var imageName: String
+    var backgroundColor: Color = Color.white
     
     
     var action: () -> Void
@@ -32,8 +33,8 @@ struct WhiteButton: View {
                 Spacer()
             }
             .padding(.vertical, 6)
-            .foregroundColor(.black)
-            .background(Color.white)
+            .foregroundColor(backgroundColor == .white ? .black : .white)
+            .background(backgroundColor)
             .cornerRadius(3.0)
             
         })
@@ -46,7 +47,7 @@ struct WhiteButton_Previews: PreviewProvider {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             
-        WhiteButton(text: "Play", imageName: "play.fill") {
+        PlayButton(text: "Play", imageName: "play.fill") {
             //
         }
         }
