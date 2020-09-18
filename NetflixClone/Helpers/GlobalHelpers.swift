804e80b49eaf907f62166f465e195cfa6e30b3b1
gianmarcog <marco.giretti@web.de>
2020-09-18 13:55:28 +0200

    Added more information for the application

--- NetflixClone/Helpers/GlobalHelpers.swift
+++ NetflixClone/Helpers/GlobalHelpers.swift
@@ -10,15 +10,84 @@ import SwiftUI
 
 //static information | Befinden sich die Daten, welche über Movie.swift aufgerufen werden.
 
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 1)
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/301")!,categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 2, promotionHeadline: "Best Rated Show")
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/302")!,categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 3)
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/303")!,categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 4, promotionHeadline: "New Episodes coming soon")
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hanniball", thumbnailURL: URL(string: "https://picsum.photos/200/304")!,categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 5)
-let exampleMovie6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/305")!,categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 6, promotionHeadline: "Watch Season 6 Now")
+let exampleMovie1 = Movie(
+    id: UUID().uuidString,
+    name: "DARK",
+    thumbnailURL: URL(string: "https://picsum.photos/200/300")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 1,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baron bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel")
+
+let exampleMovie2 = Movie(
+    id: UUID().uuidString,
+    name: "Travelers",
+    thumbnailURL: URL(string: "https://picsum.photos/200/301")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 2,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baron bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel",
+    promotionHeadline: "Best Rated Show")
+
+let exampleMovie3 = Movie(
+    id: UUID().uuidString,
+    name: "Community",
+    thumbnailURL: URL(string: "https://picsum.photos/200/302")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 3,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baron bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel")
+
+let exampleMovie4 = Movie(
+    id: UUID().uuidString,
+    name: "Alone",
+    thumbnailURL: URL(string: "https://picsum.photos/200/303")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 4,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baron bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel",
+    promotionHeadline: "New Episodes coming soon")
+
+let exampleMovie5 = Movie(
+    id: UUID().uuidString,
+    name: "Hanniball",
+    thumbnailURL: URL(string: "https://picsum.photos/200/304")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 5,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baron bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel")
+
+let exampleMovie6 = Movie(
+    id: UUID().uuidString,
+    name: "After Life",
+    thumbnailURL: URL(string: "https://picsum.photos/200/305")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 6,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baron bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel",
+    promotionHeadline: "Watch Season 6 Now")
 
 let exampleMovies: [Movie] = [exampleMovie1, exampleMovie2, exampleMovie3, exampleMovie4, exampleMovie5, exampleMovie6]
 
+let exampleEpisodeInfo1 = CurrentEpisodeInfo(episodeName: "Welcome to Miami", description: "This is the dafault text which is going to show, if this episode has no informationen. So when you gonna see this the series is realy bad. Otherhwise their would be a description.", season: 2, episode: 1)
 
 extension LinearGradient {
     static let blackOpacityGradient =  LinearGradient(
@@ -31,5 +100,13 @@ extension LinearGradient {
 struct GlobalHelpers_Previews: PreviewProvider {
     static var previews: some View {
         /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+extension String {
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
     }
 }
