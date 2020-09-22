e658a19b4da64d3270db775db7318fa3c2da09bb
gianmarcog <marco.giretti@web.de>
2020-09-22 02:58:35 +0200

    Added new File

---
+++ NetflixClone/HomeViews/HomeStack.swift
@@ -0,0 +1,59 @@
+//
+//  HomeStack.swift
+//  NetflixClone
+//
+//  Created by Gianmarco Giretti on 21.09.20.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeStack: View {
+    
+    var vm: HomeVM
+   
+    var topRowSelection: HomeTopRow
+    
+    @Binding var movieDetailToShow: Movie?
+    
+    var body: some View {
+        ForEach(vm.allCategories, id: \.self) { category in
+            //Alle Kategorien werden nochmals in einem VStach sortiert
+            VStack {
+                HStack {
+                    Text(category)
+                        .font(.title3)
+                        .bold()
+                    Spacer()
+                }
+                
+                // Damit Filme einer Kategorie horizontal gewischt werden können
+                ScrollView(.horizontal, showsIndicators: false){
+                    LazyHStack {
+                        ForEach(vm.getMovie(forCat: category, andHomeRow: topRowSelection)) { movie in
+                            StandardHomeMovie(movie: movie)
+                                .frame(width: 100, height: 200)
+                                .padding(.horizontal, 30)
+                                .onTapGesture(perform: {
+                                    movieDetailToShow = movie
+                                })
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct HomeStack_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack{
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            ScrollView{
+                HomeStack(vm: HomeVM(), topRowSelection: .movies, movieDetailToShow: .constant(nil))
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
