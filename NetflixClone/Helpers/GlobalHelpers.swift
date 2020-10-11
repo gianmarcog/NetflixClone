715818cc8dea81739de1d6dbe44ba1b381203965
gianmarcog <marco.giretti@web.de>
2020-10-11 17:26:02 +0200

    Added logic to the searchbarUI

--- NetflixClone/Helpers/GlobalHelpers.swift
+++ NetflixClone/Helpers/GlobalHelpers.swift
@@ -209,3 +209,10 @@ extension String {
         return size.width
     }
 }
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder
+                                            ), to: nil, from: nil, for: nil)
+    }
+}
--- NetflixClone/Search/SearchBar.swift
+++ NetflixClone/Search/SearchBar.swift
@@ -9,13 +9,72 @@ import SwiftUI
 
 struct SearchBar: View {
     
-    @State private var text: String = ""
+    @Binding var text: String
+    
+    @State private var isEditing = true
+    
+    @Binding var isLoading: Bool
     
     var body: some View {
-        HStack {
-            Image(systemName: "magnifyingglass")
-            TextField("Search", text: $text)
-                .background(Color.white)
+        ZStack(alignment: .leading) {
+            
+            Color.graySearchBackground
+                .frame(width: 300, height: 36)
+                .cornerRadius(8)
+            
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color.graySearchText)
+                    .padding(.leading, 10)
+                
+                TextField("Search", text: $text)
+                    .padding(7)
+                    .padding(.leading, -7)
+                    .background(Color.graySearchBackground)
+                    .cornerRadius(8)
+                    .foregroundColor(.white)
+                    .onTapGesture(perform: {
+                        isEditing = true
+                    })
+                
+                if !text.isEmpty {
+                    if isLoading {
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            ActivityIndicator(style: .medium, animate: .constant(true))
+                                .configure {
+                                    $0.color = .white
+                                }
+                        })
+                        .padding(.trailing, 32)
+                        .frame(width: 35, height: 35)
+                    } else {
+                        
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.graySearchText)
+                                .frame(width: 35, height: 35)
+                        })
+                        .padding(.trailing, 18)
+                        
+                    }
+                }
+                
+                if isEditing {
+                    Button(action: {
+                        text = ""
+                        isEditing = false
+                        hideKeyboard()
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.white)
+                    })
+                    .padding(.trailing, 20)
+                }
+            }
         }
     }
 }
@@ -26,7 +85,8 @@ struct SearchBar_Previews: PreviewProvider {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             
-            SearchBar()
+            SearchBar(text: .constant(""), isLoading: .constant(false))
+                .padding()
         }
     }
 }
