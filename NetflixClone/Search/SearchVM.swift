05a9cbfad26e03b3c5ffa7c4757893e042428236
gianmarcog <marco.giretti@web.de>
2020-10-13 16:21:26 +0200

    Added new file

---
+++ NetflixClone/Search/SearchVM.swift
@@ -0,0 +1,78 @@
+//
+//  SearchVM.swift
+//  NetflixClone
+//
+//  Created by Gianmarco Giretti on 11.10.20.
+//
+
+import Foundation
+import SwiftUI
+
+class SearchVM: ObservableObject {
+    
+    @Published var isLoading: Bool = false
+    
+    @Published var viewState: ViewState = ViewState.ready
+    
+    @Published var popularMovies: [Movie] = []
+    @Published var searchResults: [Movie] = []
+    
+    @Published var isShowingPopularMovies = true
+    
+    init() {
+        getPopularMovies()
+    }
+    
+    
+    public func updateSearchText(with text: String) {
+        setViewState(to: .loading)
+        
+        if text.count > 0 {
+            isShowingPopularMovies = false
+            getSearchResults(forText: text)
+        } else {
+            isShowingPopularMovies = true
+        }
+        
+        getSearchResults(forText: text)
+    }
+    
+    private func getPopularMovies() {
+        self.popularMovies = generateMovies(40)
+        
+    }
+    
+    private func getSearchResults(forText text: String) {
+        
+        let haveResult = Int.random(in: 0...3)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            if haveResult == 0 {
+                // empty View
+                self.searchResults = []
+                self.setViewState(to: .empty)
+            } else {
+                // ready View
+                let movies = generateMovies(21)
+                self.searchResults = movies
+                self.setViewState(to: .ready)
+            }
+            
+        }
+        
+    }
+    
+    private func setViewState(to state: ViewState) {
+        DispatchQueue.main.async {
+            self.viewState = state
+            self.isLoading = state == .loading
+        }
+    }
+}
+
+enum ViewState {
+    case empty
+    case loading
+    case ready
+    case error
+}
---
+++ NetflixClone/Search/SearchView.swift
@@ -0,0 +1,52 @@
+//
+//  SearchView.swift
+//  NetflixClone
+//
+//  Created by Gianmarco Giretti on 11.10.20.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    @ObservedObject var vm = SearchVM()
+    
+    @State private var searchText = ""
+    var body: some View {
+        
+        let searchTextBinding = Binding {
+             return searchText
+        } set: {
+            searchText = $0
+        }
+        
+        return ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                SearchBar(text: searchTextBinding, isLoading: $vm.isLoading)
+                    .padding()
+                
+                ScrollView {
+                    if vm.isShowingPopularMovies {
+                        Text("Popular Movies")
+                    }
+                    if vm.viewState == .empty {
+                        Text("Empty")
+                    } else if vm.viewState == .ready && !vm.isShowingPopularMovies {
+                        Text("Ready")
+                    }
+                    
+                    Spacer()
+                }
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
